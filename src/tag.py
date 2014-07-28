1eb7f597c140017283d0d901fc06b817aa7f76c8
Sysel <marian.sysel.hornak@gmail.com>
2014-07-28 13:06:15 +0200

    Tag registration functions

--- src/tag.py
+++ src/tag.py
@@ -11,3 +11,40 @@ class Tag:
         yield '#%s ' % self.tag
         yield from _iterencode(self.data, _current_indent_level)
 
+
+_root = {}
+
+
+def resolve(path):
+    tokens = path.split('/')
+    act = _root
+    for t in tokens:
+        if act == None:
+            return None
+        act = act.get(t)
+    return act
+
+
+def register(path, o):
+    tokens = path.split('/')
+    act = _root
+    for t in tokens[:-1]:
+        if act == None:
+            raise ValueError("Parent namespace not registered")
+        act = act.get(t)
+    if act == None:
+        raise ValueError("Parent namespace not registered")
+    if tokens[-1] in act:
+        raise ValueError("Path already in use")
+    act[tokens[-1]] = o
+
+ 
+def delete(path):
+    tokens = path.split('/')
+    act = _root
+    for t in tokens[:-1]:
+        if act == None:
+            return
+        act = act.get(t)
+    if tokens[-1] in act:
+        del act[tokens[-1]]
