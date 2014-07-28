38217274d15b700484df3701f65bf06fe3972d85
Sysel <marian.sysel.hornak@gmail.com>
2014-07-28 10:01:34 +0200

    ESON Decoder

---
+++ src/decoder.py
@@ -0,0 +1,16 @@
+""" ESONDecoder - extended JSONDeccoder
+"""
+
+from json.decoder import JSONDecoder
+from scanner import py_make_eson_scanner
+
+
+__all__ = ['ESONDecoder']
+
+
+class ESONDecoder (JSONDecoder):
+
+    def __init__(self, *args, **kwargs):
+        super(ESONDecoder, self).__init__(*args, **kwargs)
+        self.scan_once = py_make_eson_scanner(self)
+
