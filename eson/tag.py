58e6a83dbf657c7c2aaf18dc4e63def70dbb6b46
Sysel <marian.sysel.hornak@gmail.com>
2014-07-29 15:49:20 +0200

    Tag documentation

--- eson/tag.py
+++ eson/tag.py
@@ -1,9 +1,27 @@
-""" ESON Tag class
+""" ESON Tag features
+
+Provides:
+
+# Tag representation. #
+Used for encoding tags from or, optionally, decoding tags to.
+
+
+# Tag registration tools. #
+To decode tag, you need have registered it with
+handler function which transforms decoded data to final object.
+Tags can be organized in namespace tree. Tag identifiers are paths in this tree
+using slash ( '/' ) as a separator. Namespace is represented by dict object
+where values can be other namespaces or handlers.
 """
 
+
 class Tag:
+    """ Class representing ESON tag. The only way to encode tags.
+    """
 
     def __init__(self, tag, data):
+        """ Constructs new Tag object using tag string and data object.
+        """
         self.data = data
         self.tag = tag
 
@@ -19,11 +37,15 @@ class Tag:
         )
 
 
-
+""" Tag namespace root
+"""
 _root = {}
 
 
 def resolve(path):
+    """ Returns object at given path in namespace tree.
+    Returns None if path does not exist.
+    """
     tokens = path.split('/')
     act = _root
     for t in tokens:
@@ -34,6 +56,9 @@ def resolve(path):
 
 
 def register(path, o):
+    """ Registers object to given path.
+    Parent namespace have to exist but path itself not.
+    """
     tokens = path.split('/')
     act = _root
     for t in tokens[:-1]:
@@ -48,6 +73,8 @@ def register(path, o):
 
  
 def delete(path):
+    """ Deletes whole subtree of given path if exists
+    """
     tokens = path.split('/')
     act = _root
     for t in tokens[:-1]:
